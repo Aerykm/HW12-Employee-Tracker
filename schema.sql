5b465cb6ca91303d00059921c8f123ab8c20cc06
aeryk <aerykmarrero@yahoo.com>
2020-10-01 23:44:42 -0500

    Fixing schema and creating server.js

--- schema.sql
+++ schema.sql
@@ -2,24 +2,27 @@ DROP DATABASE IF EXISTS Employees_DB;
 
 CREATE DATABASE Employees_DB;
 
-USE DATABASE Employees_DB;
+USE Employees_DB;
 
 CREATE TABLE department (
-    id - INT PRIMARY KEY
-    name - VARCHAR(30)
+    id INT auto_increment,
+	name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id - INT PRIMARY KEY
-    title - VARCHAR(30)
-    salary - DECIMAL
-    department_id - INT
-)
+    id  INT auto_increment,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+     PRIMARY KEY(id)
+);
 
 CREATE TABLE employee (
-    id - INT PRIMARY KEY
-    first_name - VARCHAR(30)
-    last_name - VARCHAR(30)
-    role_id - INT
-    manager_id - INT
-)
+    id INT auto_increment,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+     PRIMARY KEY(id)
+);
